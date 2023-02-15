03045ca89e0dc90bd2a03f2b8e92eedec73e9732
milekc <93735042+milekc@users.noreply.github.com>
2023-02-15 10:34:20 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
 
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    languages = "ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb "
+    language = request.config.getoption("language")
+    if (language + " ") in languages:
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        print(
+            "\nlanguage {} not supported :(\ntry: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans "
+            "en-gb".format(
+                language))
+        pytest.fail("Wrong Language")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
