82403d7fd2ff9b31b5f9e21d5349e0d934f8f8f7
milekc <93735042+milekc@users.noreply.github.com>
2023-02-15 10:33:50 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,11 @@
+from conftest import *
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_item_add_to_basket(browser):
+    browser.get(link)
+    # time.sleep(30)
+    basket = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    assert basket.text, 'Element not found'
+    print(basket.text)
